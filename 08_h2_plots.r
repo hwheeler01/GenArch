dcdd1fa8163b2743fde7d7d460ae16b675644d34
hwheeler01 <hwheeler@bsd.uchicago.edu>
2014-11-25 16:06:25 -0600

    added png output

--- 08_h2_plots.r
+++ 08_h2_plots.r
@@ -3,14 +3,14 @@ library(scatterplot3d)
 date <- Sys.Date()
 grms<-read.table('GTEx.cross-tissue.h2.all.models_2014-11-25.txt',header=T,sep="\t")
 
-pdf(file="GTEx_cross-tissue_global_v_local_h2_" %&% date %&% ".pdf",height=5,width=5)
+pdf(file="plots/GTEx_cross-tissue_global_v_local_h2_" %&% date %&% ".pdf",height=5,width=5)
 plot(grms$local.h2,grms$global.h2,xlim=c(0,1),ylim=c(0,1),cex=0.5,xlab="localGRM h2",ylab="globalGRM h2",
      main="GTEx Cross-tissue expression GCTA",col=rgb(100,100,100,100,maxColorValue=255))
 abline(1,-1,col="red")
 dev.off()
 
 grm3<-cbind(grms$local.h2.1,grms$chr.h2,grms$global.h2.1)
-pdf(file="GTEx_cross-tissue_global_v_local_v_chr_h2_" %&% date %&% ".pdf",height=5,width=5)
+pdf(file="plots/GTEx_cross-tissue_global_v_local_v_chr_h2_" %&% date %&% ".pdf",height=5,width=5)
 scatterplot3d(grm3[,1],grm3[,2],grm3[,3],pch=16, highlight.3d=TRUE,cex.symbols=0.5,
               type="p",xlab="localGRM h2",ylab="chrGRM h2",zlab="globalGRM h2",main="GTEx Cross-tissue expression GCTA")
 dev.off()
@@ -21,14 +21,14 @@ for(tis in tissues[,1]){
   file<- "GTEx.resid.tissue-specific.h2_" %&% tis %&% "_all.models_2014-11-24.txt"
   tisgrms<-read.table(file,header=T,sep="\t")
   
-  pdf(file="GTEx_resid_" %&% tis %&% "_global_v_local_h2_" %&% date %&% ".pdf",height=5,width=5)
+  pdf(file="plots/GTEx_resid_" %&% tis %&% "_global_v_local_h2_" %&% date %&% ".pdf",height=5,width=5)
   plot(tisgrms$local.h2,tisgrms$global.h2,xlim=c(0,1),ylim=c(0,1),cex=0.5,xlab="localGRM h2",ylab="globalGRM h2",
        main="GTEx " %&% tis %&% " expression GCTA",col=rgb(100,100,100,100,maxColorValue=255))
   abline(1,-1,col="red")
   dev.off()
   
   tisgrm3<-cbind(tisgrms$local.h2.1,tisgrms$chr.h2,tisgrms$global.h2.1)
-  pdf(file="GTEx_resid_" %&% tis %&% "_global_v_local_v_chr_h2_" %&% date %&% ".pdf",height=5,width=5)
+  pdf(file="plots/GTEx_resid_" %&% tis %&% "_global_v_local_v_chr_h2_" %&% date %&% ".pdf",height=5,width=5)
   scatterplot3d(tisgrm3[,1],tisgrm3[,2],tisgrm3[,3],pch=16, highlight.3d=TRUE,cex.symbols=0.5,
                 type="p",xlab="localGRM h2",ylab="chrGRM h2",zlab="globalGRM h2",main="GTEx " %&% tis %&% " expression GCTA")
   dev.off()
@@ -37,7 +37,7 @@ for(tis in tissues[,1]){
 ## Y ~ localGRM
 ###plot histograms of h2
 
-png(file="hist_GTEx_localGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
 par(mfcol=c(2,4))
 ###no ylim
 hist(grms$h2,ylim=c(0,15000),xlim=c(0,1),main="Cross-tissue",xlab="h2")
@@ -48,7 +48,7 @@ for(tis in tissues[,1]){
 }
 dev.off()
 ###ylim = 3000
-png(file="hist_GTEx_localGRM_h2_ylim3000_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_h2_ylim3000_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
 par(mfcol=c(2,4))
 hist(grms$h2,ylim=c(0,3000),xlim=c(0,0.5),main="Cross-tissue",xlab="h2")
 for(tis in tissues[,1]){
@@ -59,7 +59,7 @@ for(tis in tissues[,1]){
 dev.off()
 
 ###plot histograms of p
-png(file="hist_GTEx_localGRM_p_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_p_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
 par(mfcol=c(2,4))
 ###no ylim
 hist(grms$p,main="Cross-tissue",xlab="p-value",ylim=c(0,10000))
@@ -73,7 +73,7 @@ dev.off()
 ## Y ~ localGRM + globalGRM
 ###plot histograms of h2
 
-png(file="hist_GTEx_localGRM_globalGRM_h2_" %&% date %&% ".png",height=10,width=10,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_globalGRM_h2_" %&% date %&% ".png",height=10,width=10,units="in",res=100)
 par(mfcol=c(4,4))
 ###ylim = 2000
 hist(grms$local.h2,ylim=c(0,2000),xlim=c(0,1),main="Cross-tissue",xlab="localGRM h2")
@@ -91,7 +91,7 @@ for(tis in tissues[,1]){
 dev.off()
 
 ###plot histograms of se
-png(file="hist_GTEx_localGRM_globalGRM_se_" %&% date %&% ".png",height=10,width=10,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_globalGRM_se_" %&% date %&% ".png",height=10,width=10,units="in",res=100)
 par(mfcol=c(4,4))
 ###ylim = 5000
 hist(grms$local.se,xlim=c(0,1),main="Cross-tissue",xlab="localGRM SE",ylim=c(0,5000))
@@ -111,7 +111,7 @@ dev.off()
 ## Y ~ localGRM + chrGRM + globalGRM
 ###plot histograms of h2
 
-png(file="hist_GTEx_localGRM_chrGRM_globalGRM_h2_" %&% date %&% ".png",height=10,width=15,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_chrGRM_globalGRM_h2_" %&% date %&% ".png",height=10,width=15,units="in",res=100)
 par(mfcol=c(4,6))
 ###ylim = 2000
 hist(grms$local.h2.1,ylim=c(0,2000),xlim=c(0,1),main="Cross-tissue",xlab="localGRM h2")
@@ -135,7 +135,7 @@ for(tis in tissues[,1]){
 dev.off()
 
 ###plot histograms of se
-png(file="hist_GTEx_localGRM_chrGRM_globalGRM_se_" %&% date %&% ".png",height=10,width=15,units="in",res=100)
+png(file="plots/hist_GTEx_localGRM_chrGRM_globalGRM_se_" %&% date %&% ".png",height=10,width=15,units="in",res=100)
 par(mfcol=c(4,6))
 ###ylim = 4000
 hist(grms$local.se.1,xlim=c(0,1),main="Cross-tissue",xlab="localGRM SE",ylim=c(0,4000))
@@ -160,7 +160,7 @@ dev.off()
 
 
 ###plot scatterplots global v. local h2 on one png
-png(file="scatter_GTEx_localGRM_globalGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
+png(file="plots/scatter_GTEx_localGRM_globalGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
 par(mfcol=c(2,4))
 ###no ylim
 plot(grms$local.h2,grms$global.h2,xlim=c(0,1),ylim=c(0,1),cex=0.5,xlab="localGRM h2",ylab="globalGRM h2",
@@ -176,7 +176,7 @@ for(tis in tissues[,1]){
 dev.off()
 
 ###plot 3D scatterplots global v. local v. chr h2 on one png
-png(file="scatter_GTEx_localGRM_chrGRM_globalGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
+png(file="plots/scatter_GTEx_localGRM_chrGRM_globalGRM_h2_" %&% date %&% ".png",height=6,width=10,units="in",res=100)
 par(mfcol=c(2,4))
 ###no ylim
 scatterplot3d(grm3[,1],grm3[,2],grm3[,3],pch=16, highlight.3d=TRUE,cex.symbols=0.5,cex.lab=0.7,cex.axis=0.5,xlim=c(0,1),ylim=c(0,1),zlim=c(0,1),
