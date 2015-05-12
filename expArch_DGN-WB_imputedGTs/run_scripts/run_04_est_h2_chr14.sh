30b5f1e87369758a563c51a0d953a69e4a445264
hwheeler01 <hwheeler@bsd.uchicago.edu>
2015-05-12 15:37:45 -0500

    changed permissions again

---
+++ expArch_DGN-WB_imputedGTs/run_scripts/run_04_est_h2_chr14.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N R.gcta.14
+#PBS -S /bin/bash
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=10gb
+#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
+cd $PBS_O_WORKDIR
+
+module load R
+module load gcta/1.24.4
+
+time R --no-save < 04_est_h2.r --args 14
---
+++ expArch_DGN-WB_imputedGTs/run_scripts/run_06_calc_knownTrans_GRMs_12.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N R.gcta.12
+#PBS -S /bin/bash
+#PBS -l walltime=100:00:00
+#PBS -l nodes=1:ppn=8
+#PBS -l mem=10gb
+#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
+cd $PBS_O_WORKDIR
+
+module load R
+module load gcta/1.24.4
+
+time R --no-save < 06_calc_knownTrans_GRMs.r --args 12
