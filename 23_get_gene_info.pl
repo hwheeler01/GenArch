86176eb76a5fd08988c19c52dd10721ac55981a3
hwheeler01 <hwheeler@bsd.uchicago.edu>
2015-05-20 12:29:10 -0500

    get GTEx h2 se geneinfo

---
+++ 23_get_gene_info.pl
@@ -0,0 +1,26 @@
+use warnings;
+use strict;
+
+my %info;
+
+open(A, "GENCODE_GRCh37.p12_Ensembl73.txt");
+while(<A>){
+    chomp;
+    my ($ensid,$descr,$gene) = split(/\t/);
+    $info{$ensid} = $descr . "\t" . $gene;
+}
+
+open(B, "GTEx_Tissue-Specific_local_h2_se.txt");
+open(OUT, ">GTEx_Tissue-Specific_local_h2_se_geneinfo.txt");
+
+while(defined(my $line=<B>)){
+    my ($e) = split(/\t/,$line);
+    my ($ensid) = split(/\./,$e);
+    my $geneinfo;
+    if(defined($info{$ensid})){
+	$geneinfo = $info{$ensid};
+    }else{
+	$geneinfo = "NA\tNA";
+    }
+    print OUT "$geneinfo\t$line";
+}
